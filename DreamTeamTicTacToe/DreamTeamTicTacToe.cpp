c2d39d84f86383563e7c394475d8667e857ffc8c
John Syvertsen <johnaylwyn@proton.me>
2025-09-14 22:42:15 -0400

    DREAM-7 inital commit

--- DreamTeamTicTacToe/DreamTeamTicTacToe.cpp
+++ DreamTeamTicTacToe/DreamTeamTicTacToe.cpp
@@ -2,10 +2,11 @@
 //
 
 #include <iostream>
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    std::cout << "Hello, Tic-tac-toe!\n";
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
